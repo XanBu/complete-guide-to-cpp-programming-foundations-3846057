6c194432fe4f31cc997b1d89c20fbfafe36f9280
Xander Budge <83941337+XanBu@users.noreply.github.com>
2025-10-29 02:35:01 +0000

    ch4. Arrays, Pointers, Strings exercises

--- src/Ch04/04_01b/CodeDemo.cpp
+++ src/Ch04/04_01b/CodeDemo.cpp
@@ -4,8 +4,24 @@
 
 #include <iostream>
 
-int main(){
-    
+//#define LEVEL_COUNT 4
+
+int main(){ 
+    const size_t LEVEL_COUNT = 4;
+    int enemies[LEVEL_COUNT];
+    //compiler will assign float to doubles, implicit conversion
+    //auto doesn'r work for arrays, type needs to be defined
+    float levelDifficulty[] = {1.0,3.5,6.0,10.5}; 
+
+    enemies[0] = 10;
+    enemies[1] = 15;
+    enemies[2] = 20;
+    enemies[3] = 25;
+
+    std::cout << "Level 1: "  << enemies[0] << " enemies" << std::endl;
+    std::cout << "Level Difficulty: "  << levelDifficulty[0] << std::endl;
+
+
     std::cout << std::endl << std::endl;
     return 0;
 }
